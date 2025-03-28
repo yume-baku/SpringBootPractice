7ba077cb8e33393a41e11dc02bba9035fd6f8bca
yume <masato.umeno.818@gmail.com>
2025-03-28 21:42:31 +0900

    ディレクトリ整理(contactとadminの分割。contact.html→index.htmlに命名変更。re

--- practice/src/main/java/com/example/practice/contact/service/ContactServiceImpl.java
+++ practice/src/main/java/com/example/practice/contact/service/ContactServiceImpl.java
@@ -1,11 +1,11 @@
-package com.example.demo.service;
+package com.example.practice.contact.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Contact;
-import com.example.demo.form.ContactForm;
-import com.example.demo.repository.ContactRepository;
+import com.example.practice.contact.entity.Contact;
+import com.example.practice.contact.form.ContactForm;
+import com.example.practice.contact.repository.ContactRepository;
 
 @Service
 public class ContactServiceImpl implements ContactService {

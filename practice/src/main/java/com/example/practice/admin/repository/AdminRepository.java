67dd1a5361c7da244acc5b8b1a1ab77d51f97f60
yume <masato.umeno.818@gmail.com>
2025-03-30 18:50:47 +0900

    最終課題 その4

--- practice/src/main/java/com/example/practice/admin/repository/AdminRepository.java
+++ practice/src/main/java/com/example/practice/admin/repository/AdminRepository.java
@@ -1,9 +1,12 @@
 package com.example.practice.admin.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.practice.admin.entity.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
+	Optional<Admin> findByEmailEquals(String email);
 }
